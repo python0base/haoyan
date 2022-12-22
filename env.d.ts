1347e607569e41d5aacb017768e9e9c8987ec903
yanhao <1134541910@qq.com>
2022-12-22 10:18:51 +0800

    a

--- env.d.ts
+++ env.d.ts
@@ -1,9 +1,11 @@
 /// <reference types="vite/client" />
-
+// 类型定义和声明文件
 declare module '*.vue' {
   import { DefineComponent } from 'vue'
   const component: DefineComponent
   export default component
+  interface IModalConfig {}
+  export { IModalConfig }
 }
 
 declare module '*.mjs'
